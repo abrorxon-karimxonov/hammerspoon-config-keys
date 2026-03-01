b64df7a73485a3bd5588edf1df2160b956f67054
Abror <abrorxon158@gmail.com>
2026-03-01 15:54:49 +0500

    add mac monitor app open

--- config/apps.lua
+++ config/apps.lua
@@ -3,9 +3,10 @@
 
 return {
     { id = "com.apple.systempreferences",   key = "s" },
+    { id = "com.apple.ActivityMonitor",     key = "m" },
     { id = "com.apple.finder",              key = "e" },
-    { id = "com.microsoft.VSCode", key = "c" }, -- visual code idea
-    -- { id = "com.todesktop.230313mzl4w4u92", key = "c" }, -- cursor idea
+    { id = "com.microsoft.VSCode",          key = "c" }, -- visual code idea
+-- { id = "com.todesktop.230313mzl4w4u92",  key = "c" }, -- cursor idea
     { id = "app.zen-browser.zen",           key = "b" },
     { id = "ru.keepcoder.Telegram",         key = "t" },
     { id = "com.mitchellh.ghostty",         key = "g" },
